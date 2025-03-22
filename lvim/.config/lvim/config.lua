c5f06783fc5ac7375ebbd98d8636a86dccefedb1
Gui Ferreira <guifl_pereira@hotmail.com>
2025-03-22 19:08:07 -0300

    Adicionando configurações para o java no lvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -123,7 +123,7 @@ lvim.plugins = {
     --       cmd = "TroubleToggle",
   },
 }
-
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 -- vim.api.nvim_create_autocmd("FileType", {
 --   pattern = "zsh",
